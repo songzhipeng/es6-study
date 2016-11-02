442b79643dc08c4d274bf8d6304b4f9a24f6e42a
zero <songzhipeng@me.com>
2016-11-02 20:09:09 +0800

    no message

--- note/test.js
+++ note/test.js
@@ -1,3 +1,21 @@
 'use strict';
 
-console.log(typeof(undefined));
+function throwIfMissing() {
+  throw new Error('Missing parameter');
+}
+
+function throwIfParameterTypeNotEqual(param, type) {
+  if (typeof(param) !== type) {
+    throw new Error('Parameter type error')
+  }
+}
+
+function func(n = throwIfMissing()) {
+  throwIfParameterTypeNotEqual(n, 'number');
+  return n*n;
+}
+
+
+console.log(func(3));
+console.log(func()); // 报错
+console.log(func('1')); // 报错
